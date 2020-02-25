1471e67640058c409cb431e67cd3c67ee7023c87
Marin <Marin35@users.noreply.github.com>
2020-02-25 09:31:11 +0100

    Refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,7 @@ export class AppComponent {
   // Initialize Firebase
   constructor() {
     const config = {
-      apiKey: "AIzaSyCltw98az9ydn0aVngNMNVVAKZEHSdDM9s",
-      authDomain: "gnothi-30f59.firebaseapp.com",
-      databaseURL: "https://gnothi-30f59.firebaseio.com",
-      projectId: "gnothi-30f59",
-      storageBucket: "gnothi-30f59.appspot.com",
-      messagingSenderId: "76859783078"
+      ""
     };
     firebase.initializeApp(config);
   }
